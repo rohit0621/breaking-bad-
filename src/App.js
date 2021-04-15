1ae226cf8e55ba2c63c4a17cd50bab8976667bd5
Apple <63094930+rohit0621@users.noreply.github.com>
2021-04-15 19:13:02 +0530

    Initial commit - react app for breaking bad character search

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/ui/Header";
+import Search from "./components/ui/Search";
+import axios from "axios";
+import CharacterGrid from "./components/characters/CharacterGrid";
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true);
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+      // console.log(result.data, "fdsf");
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
+  return (
+    <div className="container">
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid items={items} isLoading={isLoading} />
+    </div>
+  );
+};
+
+export default App;
